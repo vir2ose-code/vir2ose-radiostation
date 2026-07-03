298738fc63a9ebd4b6f2db05bc5ee61792ffeb09
Waldemar Krucinski <vir2ose@gmail.com>
2026-07-03 23:46:06 +0200

    Update manifest.json for PWABuilder compliance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vir2ose-radio-v3';
+const CACHE_NAME = 'vir2ose-radio-v4';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -7,7 +7,8 @@ const ASSETS_TO_CACHE = [
   './manifest.json',
   './RADIO ONLINE BACK 1.png',
   './vir2ose LOGO TRANS.png',
-  './Online Radio fm V4.png'
+  './Online Radio fm V4.png',
+  './512x512 PING.png'
 ];
 
 // Install Event: App-Shell cachen
