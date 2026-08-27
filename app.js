3fc3c0a37f854c4d9a06945843765da65c35397f
Waldemar Krucinski <radio@waldemar-krucinski.com>
2026-08-27 21:32:03 +0200

    Update AI folder to AI-Supported_Music and bump cache version

--- app.js
+++ app.js
@@ -97,17 +97,17 @@ const genrePlaylists = {
         "AUDIO-LIBRARY/GENRES/PSYCHODELIC/PASSIONATELY - EOV by vir2ose 2025.mp3",
         "AUDIO-LIBRARY/GENRES/PSYCHODELIC/PHANTOM OF THE EDM by vir2ose 2025.mp3"
     ],
-    "AI_Support": [
-        "AUDIO-LIBRARY/GENRES/AI_Support/1-22.08.26.mp3",
-        "AUDIO-LIBRARY/GENRES/AI_Support/1_27.08.26.mpeg",
-        "AUDIO-LIBRARY/GENRES/AI_Support/2-22.08.26.mp3",
-        "AUDIO-LIBRARY/GENRES/AI_Support/2_27.08.26.mpeg",
-        "AUDIO-LIBRARY/GENRES/AI_Support/3-22.08.26.mp3",
-        "AUDIO-LIBRARY/GENRES/AI_Support/3_27.08.26.mpeg",
-        "AUDIO-LIBRARY/GENRES/AI_Support/4-22.08.26.mp3",
-        "AUDIO-LIBRARY/GENRES/AI_Support/4_27.08.26.mpeg",
-        "AUDIO-LIBRARY/GENRES/AI_Support/5_22.08.26.mp3",
-        "AUDIO-LIBRARY/GENRES/AI_Support/5_27.08.26.mpeg"
+    "AI_Supported_Music": [
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/1-22.08.26.mp3",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/1_27.08.26.mpeg",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/2-22.08.26.mp3",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/2_27.08.26.mpeg",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/3-22.08.26.mp3",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/3_27.08.26.mpeg",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/4-22.08.26.mp3",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/4_27.08.26.mpeg",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/5_22.08.26.mp3",
+        "AUDIO-LIBRARY/GENRES/AI_Supported_Music/5_27.08.26.mpeg"
     ],
     "EDM": [
         "AUDIO-LIBRARY/GENRES/EDM/ADVENTURE WORLD by vir2ose 2024.mp3",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vir2ose-radio-v2';
+const CACHE_NAME = 'vir2ose-radio-v3';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
